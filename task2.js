ff75dedd43a0e563939b93e06bc34fb1c1506c3c
Richard <therafi@yandex.ru>
2022-02-12 12:09:50 +0300

    task2

--- task2.js
+++ task2.js
@@ -1,4 +1,5 @@
-let n=6.67
+let n=6.67;
+let m=667n;
 let name='Stalin'
 let str="Hallo"
 let phrase=`Privet po nemecki = ${str}`;
@@ -6,9 +7,19 @@ console.log(phrase);
 console.log(`Privet, ${name}`);
 let user ={
             name:"Iosif",
-            age:54,
+            age:undefined,
             nation:"gruzin"};
+function function1(){};
+console.log(typeof function1);
 console.log(typeof user.nation);
 console.log(typeof n);
 let arr=[];
 console.log(typeof arr);
+console.log(typeof user.age);
+console.log(typeof m);
+let islesser=7<3;
+console.log(typeof islesser);
+let chislo=null;
+console.log(chislo);
+let id=Symbol("id54");
+console.log(typeof id);
