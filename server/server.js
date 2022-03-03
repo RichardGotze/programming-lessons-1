58da54ede1261c8350e0ad9a6daf315c81828a8d
Richard <therafi@yandex.ru>
2022-03-03 09:39:00 +0300

    Добавляет маршрутизацию

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
 const http = require("http");
 const requestHandler = (request, response) => {
-response.end("Hello world!");
+response.setHeader("Content-Type", "text/html");
+response.write("<!DOCTYPE html>");
+response.write("<html>");
+response.write("<head>");
+response.write("<title>Hello Node.js</title>");
+response.write("<meta charset=\"utf-8\" />");
+response.write("</head>");
+response.write("<body><h2>Привет</h2></body>");
+response.write("</html>");
+response.end();
 };
 http.createServer(requestHandler).listen(3000);
