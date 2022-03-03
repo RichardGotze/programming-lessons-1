437e604cc20e78a9d7a62d9c54bb9086a7658a74
Richard <therafi@yandex.ru>
2022-03-03 17:04:48 +0300

    Добавляет фото на веб-сервер и обработчик

--- server1/server.js
+++ server1/server.js
@@ -8,21 +8,22 @@ const requestHandler = (request, response) => {
         response.write("<h2>About</h2>");
     } else if (request.url == "/contact") {
         response.write("<h2>Contacts</h2>");
+    } else if (request.url == "/images/1") {
+        let content = fs.readFileSync("1.jpg", "binary");
+        response.end(content);
+    } else if (request.url == "/image/1") {
+        response.end("<img src='http://localhost:3000/images/1'/>")
     } else {
         response.write("<h2>Not found</h2>");
     }
-    response.end();
     console.log("Url: " + request.url);
     console.log("Тип запроса: " + request.method);
     console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
     console.log(request.headers);
     let user = JSON.stringify(request.headers);
 
     fs.appendFileSync("log.txt", request.url);
     fs.appendFileSync("log.txt", request.method);
     fs.appendFileSync("log.txt", user);
-    console.log("Запись файла завершена. Содержимое файла:");
-
 };
 http.createServer(requestHandler).listen(3000);
